7a3e878feb535d28e73aa5fa57e0a6276ef5762c
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2021-04-24 09:55:45 -0400

    InputManager

---
+++ Assets/Scripts/Debug/IncrementScore_Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class IncrementScore_Test : MonoBehaviour
+{
+
+    [SerializeField]
+    private IntGameEvent scoreEvent;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void b_IncrementScore()
+    {
+        score++;
+        scoreEvent.Raise(score);
+    }
+}
+
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using ScriptableObjectArchitecture;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameEvent inhaleCtrl;
+    [SerializeField]
+    private GameEvent exhaleCtrl;
+
+    [SerializeField]
+    private BoolReference lClick;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            inhaleCtrl.Raise();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            exhaleCtrl.Raise();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            lClick.Value = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            lClick.Value = false;
+        }
+    }
+}
