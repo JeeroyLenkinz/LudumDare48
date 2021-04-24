20d938e61c875314e5c5f75750a2fb72b3b19211
JeeroyLenkinz <rdotsikas@gmail.com>
2021-04-24 09:57:46 -0400

    Created Breather Script and RyanPlayground scene

---
+++ Assets/Scripts/Breather.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breather : MonoBehaviour
+{
+    private bool inhaling;
+    private bool exhaling;
+
+    [SerializeField]
+    private float breathMarkerPosition;
+
+    public float breathMarkerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inhaling = false;
+        exhaling = false;
+    }
+
+    public void e_toggleInhale() {
+        inhaling = true;
+        exhaling = false;
+    }
+
+    public void e_toggleExhale() {
+        exhaling = true;
+        inhaling = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inhaling) {
+            breathMarkerPosition += breathMarkerSpeed * Time.deltaTime;
+        }
+        else if (exhaling) {
+            breathMarkerPosition -= breathMarkerSpeed * Time.deltaTime;
+        }
+    }
+}
