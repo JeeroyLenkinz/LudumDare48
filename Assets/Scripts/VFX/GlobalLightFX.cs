a05e40af245191a78a968f723b75696e202ddcc7
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2021-04-25 17:50:49 -0400

    FX Boiii

---
+++ Assets/Scripts/VFX/GlobalLightFX.cs
@@ -0,0 +1,61 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using DG.Tweening;
+
+public class GlobalLightFX : MonoBehaviour
+{
+    public Light2D globalLight;
+    float startIntensityFloat;
+    float intensityFloat;
+    public bool isAnimating = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startIntensityFloat = globalLight.intensity;
+        intensityFloat = globalLight.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAnimating)
+        {
+            globalLight.intensity = intensityFloat;
+        }
+
+    }
+
+    public void e_FlickerOff()
+    {
+        StartCoroutine(FlickerOff());
+    }
+
+    private IEnumerator FlickerOff()
+    {
+        isAnimating = true;
+        Tween tweener = DOTween.To(() => intensityFloat, x => intensityFloat = x, 0.1f, 4f).SetEase(Ease.InOutBounce, 1f);
+
+        yield return tweener.WaitForCompletion();
+
+        isAnimating = false;
+    }
+
+    public void e_FlickerOn()
+    {
+        StartCoroutine(FlickerOn());
+    }
+
+    private IEnumerator FlickerOn()
+    {
+        isAnimating = true;
+        Tween tweener = DOTween.To(() => intensityFloat, x => intensityFloat = x, startIntensityFloat, 4f).SetEase(Ease.InOutBounce, 1f);
+
+        yield return tweener.WaitForCompletion();
+
+        isAnimating = false;
+    }
+}
