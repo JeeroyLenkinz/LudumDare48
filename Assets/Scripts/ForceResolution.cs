c1235f44980e16b886114fd892b04ef695750531
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2021-04-26 21:27:18 -0400

    forceRes

---
+++ Assets/Scripts/ForceResolution.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceResolution : MonoBehaviour
+{
+    private void Start()
+    {
+        // Start is called before the first frame update
+        Screen.SetResolution(1080, 720, true);
+    }
+
+}
